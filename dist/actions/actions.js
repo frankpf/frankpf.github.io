e026f491423a29f58a3822639f939bb6478d8c65
Frank Filho <me@frankpf.com>
2020-09-19 20:12:11 -0300

    testing out this actions thingy

---
+++ dist/actions/actions.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = __importDefault(require("@actions/core"));
+const github_1 = __importDefault(require("@actions/github"));
+try {
+    const nameToGreet = core_1.default.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}`);
+    const time = new Date().toTimeString();
+    core_1.default.setOutput("time", time);
+    const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
+    console.log(`Event payload: ${payload}`);
+}
+catch (err) {
+    core_1.default.setFailed(err.message);
+}
+//# sourceMappingURL=actions.js.map
---
+++ src/actions/actions.ts
@@ -0,0 +1,15 @@
+import core from '@actions/core'
+import github from '@actions/github'
+
+try {
+    const nameToGreet = core.getInput('who-to-greet')
+    console.log(`Hello ${nameToGreet}`)
+
+    const time = new Date().toTimeString() 
+    core.setOutput("time", time)
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`Event payload: ${payload}`)
+} catch(err) {
+    core.setFailed(err.message)
+}
